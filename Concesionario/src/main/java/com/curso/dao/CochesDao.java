181b159471a0d49189d1da7af04a5a1a1b8b046b
Rengoan <91058472+Rengoan@users.noreply.github.com>
2023-05-26 17:07:13 +0200

    Cambio repositorio

--- Concesionario/src/main/java/com/curso/dao/CochesDao.java
+++
@@ -1,9 +0,0 @@
-package com.curso.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.curso.model.Coche;
-
-public interface CochesDao extends JpaRepository<Coche, String> {
-
-}
