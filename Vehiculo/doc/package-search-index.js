e7ab55e851a472d5b32968290a2232307444545a
Rengoan <91058472+Rengoan@users.noreply.github.com>
2023-05-29 13:52:59 +0200

    Actualizacion javadoc

--- Vehiculo/doc/package-search-index.js
+++ Vehiculo/doc/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"es.java.vehiculo.principal"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"es.java.interfaces"},{"l":"es.java.vehiculo"},{"l":"es.java.vehiculo.principal"}];updateSearchResults();
